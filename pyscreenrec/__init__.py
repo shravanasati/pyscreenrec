15d977c8914aae2c441d280fbc8b326cc2a6c192
Shravan Asati <dev.shravan@protonmail.com>
2021-04-21 22:25:47 +0530

    🔨 Added __repr__ function and introduced HighFPSWarning class

--- pyscreenrec/__init__.py
+++ pyscreenrec/__init__.py
@@ -17,6 +17,9 @@ class InvalidStartMode(Exception):
 class InvalidFPS(Exception):
     pass
 
+class HighFPSWarning(Warning):
+    pass
+
 class ScreenRecorder:
     """
     Base class for screen recording.
@@ -26,9 +29,9 @@ class ScreenRecorder:
         """
         Constructor.
         """
-        self._running = False
-        self._start_mode = "start"
-        self.screenshot_folder = os.path.join(os.path.expanduser("~"), "Documents", "pyscreenrec_data")
+        self.__running = False
+        self.__start_mode = "start"
+        self.screenshot_folder = os.path.join(os.path.expanduser("~"), "pyscreenrec_data")
 
         # making the screenshot directory if not exists
         if not os.path.exists(self.screenshot_folder):
@@ -37,104 +40,111 @@ class ScreenRecorder:
         self._clear_data()
 
 
-    def _start_recording(self, video_name:str, fps:int):
+    def _start_recording(self, video_name:str, fps:int) -> None:
         """
         (Protected) Starts screen recording.
 
         @params 
         
-        video_name --> The name of the screen recording video.
+        video_name (str) --> The name of the screen recording video.
 
-        fps --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
+        fps (int) --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
         """
         # checking for video extension and fps
         if not video_name.endswith(".mp4"):
             raise InvalidCodec("The video's extension can only be '.mp4'.")
         if fps > 60:
             raise InvalidFPS("The FPS for the screen recording can be maximum 60 FPS.")
+        if fps > 30:
+            raise HighFPSWarning(f"The FPS you have used for screen recording, {fps}, is pretty high and may result in delayed execution of other programs in most systems. It is advised to keep it under 20.")
+
         self.fps = fps
         self.video_name = video_name
 
         # checking if screen is already being recorded
-        if self._running:
+        if self.__running:
             print("Screen recording is already running.")
 
         else:
-            if self._start_mode == "start":
-                self._running = True
+            if self.__start_mode == "start":
+                self.__running = True
                 i = 1
 
                 # starting screenshotting
-                while self._running:
+                while self.__running:
                     screenshot(os.path.join(self.screenshot_folder, f"s{i}.jpg"))
                     sleep(1/self.fps)
                     i += 1
 
-            elif self._start_mode == "resume":
-                self._running = True
+            elif self.__start_mode == "resume":
+                self.__running = True
                 i = len(natsorted([img for img in os.listdir(self.screenshot_folder) if img.endswith(".jpg")])) + 1
 
-                while self._running:
+                while self.__running:
                     screenshot(os.path.join(self.screenshot_folder, f"s{i}.jpg"))
                     sleep(1/self.fps)
                     i += 1
 
             else:
-                raise InvalidStartMode("The `self._start_mode` can only be 'start' or 'resume'.")
+                raise InvalidStartMode("The `self.__start_mode` can only be 'start' or 'resume'.")
 
 
-    def start_recording(self, video_name:str="Recording.mp4", fps:int=15):
+    def start_recording(self, video_name:str="Recording.mp4", fps:int=15) -> None:
         """
         Starts screen recording.
 
         @params 
         
-        video_name --> The name of the output screen recording.
+        video_name (str) --> The name of the output screen recording.
 
-        fps --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
+        fps (int) --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
         """
         t = Thread(target=self._start_recording, args=(video_name,fps))
         t.start()
 
-    def stop_recording(self):
+    def stop_recording(self) -> None:
         """
         Stops screen recording.
         """
-        if not self._running:
+        if not self.__running:
             print("No screen recording session is going on.")
             return None
-        self._running = False
+        self.__running = False
 
         # saving the video and clearing all screenshots
         self._save_video(self.video_name)
         self._clear_data()
 
 
-    def pause_recording(self):
+    def pause_recording(self) -> None:
         """
         Pauses screen recording.
         """
-        if not self._running:
+        if not self.__running:
             print("No screen recording session is going on.")
             return None
 
-        self._running = False
+        self.__running = False
 
-    def resume_recording(self):
+    def resume_recording(self) -> None:
         """
         Resumes screen recording.
         """
-        if self._running:
+        if self.__running:
             print("Screen recording is already running.")
             return None
 
-        self._start_mode = "resume"
+        self.__start_mode = "resume"
         self.start_recording(self.video_name)
 
 
-    def _save_video(self, video_name:str):
+    def _save_video(self, video_name:str) -> None:
         """
-        Makes a video out of the screenshots.
+        (Protected) Makes a video out of the screenshots.
+
+        @params
+
+        video_name (str) --> Name or path to the output video.
         """
         # fetching image info
         images = natsorted([img for img in os.listdir(self.screenshot_folder) if img.endswith(".jpg")])
@@ -152,10 +162,13 @@ class ScreenRecorder:
         cv2.destroyAllWindows()
         video.release()
 
-    def _clear_data(self):
+    def _clear_data(self) -> None:
         """
-        Deletes all screenshots present in the screenshot folder taken during screen recording.
+        (Protected) Deletes all screenshots present in the screenshot folder taken during screen recording.
         """
         # deleting all screenshots present in the screenshot directory
-        for screenshot in os.listdir(self.screenshot_folder):
-            os.remove(os.path.join(self.screenshot_folder, screenshot))
+        for image in os.listdir(self.screenshot_folder):
+            os.remove(os.path.join(self.screenshot_folder, image))
+
+    def __repr__(self) -> str:
+        return "pyscreenrec is a small and cross-platform python library that can be used to record screen. \nFor more info, visit https://github.com/Shravan-1908/pyscreenrec#readme."
