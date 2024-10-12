4ae44430c568c73575fd3d9c1b38b689ae17527d
Shravan Asati <dev.shravan@protonmail.com>
2024-10-12 22:03:37 +0530

    implement @damies13's fix for more frames

--- pyscreenrec/__init__.py
+++ pyscreenrec/__init__.py
@@ -1,24 +1,23 @@
 # type: ignore
 from pyscreeze import screenshot
 from time import sleep
+
 # type: ignore
 import cv2
 import os
 from threading import Thread
+
 # type: ignore
 from natsort import natsorted
+
 
 class InvalidCodec(Exception):
     pass
 
+
 class InvalidStartMode(Exception):
     pass
 
-class InvalidFPS(Exception):
-    pass
-
-class HighFPSWarning(Warning):
-    pass
 
 class ScreenRecorder:
     """
@@ -31,7 +30,9 @@ class ScreenRecorder:
         """
         self.__running = False
         self.__start_mode = "start"
-        self.screenshot_folder = os.path.join(os.path.expanduser("~"), "pyscreenrec_data")
+        self.screenshot_folder = os.path.join(
+            os.path.expanduser("~"), "pyscreenrec_data"
+        )
 
         # making the screenshot directory if not exists
         if not os.path.exists(self.screenshot_folder):
@@ -39,13 +40,12 @@ class ScreenRecorder:
         # clearing all the previous data if last session ended unsuccessfully
         self._clear_data()
 
-
-    def _start_recording(self, video_name:str, fps:int) -> None:
+    def _start_recording(self, video_name: str, fps: int) -> None:
         """
         (Protected) Starts screen recording.
 
-        @params 
-        
+        @params
+
         video_name (str) --> The name of the screen recording video.
 
         fps (int) --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
@@ -53,16 +53,14 @@ class ScreenRecorder:
         # checking for video extension and fps
         if not video_name.endswith(".mp4"):
             raise InvalidCodec("The video's extension can only be '.mp4'.")
-        if fps > 60:
-            raise InvalidFPS("The FPS for the screen recording can be maximum 60 FPS.")
-        if fps > 30:
-            raise HighFPSWarning(f"The FPS you have used for screen recording, {fps}, is pretty high and may result in delayed execution of other programs in most systems. It is advised to keep it under 20.")
 
         self.fps = fps
         self.video_name = video_name
 
         # checking if screen is already being recorded
         if self.__running:
+            # todo decide the behavior on such cases
+            # whether an error/warning should be raised or ignored
             print("Screen recording is already running.")
 
         else:
@@ -72,34 +70,56 @@ class ScreenRecorder:
 
                 # starting screenshotting
                 while self.__running:
+                    st_start = time.perf_counter()
                     screenshot(os.path.join(self.screenshot_folder, f"s{i}.jpg"))
-                    sleep(1/self.fps)
+                    st_end = time.perf_counter()
+                    st_total = st_end - st_start
+                    # not sleeping for exactly 1/self.fps seconds because
+                    # otherwise time is lost in sleeping which could be used in
+                    # capturing frames
+                    # since due to thread context-switching, this screenshotter
+                    # thread doesn't get all the time that it needs
+                    # thus, if more than required time has been spent just on
+                    # screenshotting, don't sleep at all
+                    sleep(max(0, 1 / self.fps - st_total))
                     i += 1
 
             elif self.__start_mode == "resume":
                 self.__running = True
-                i = len(natsorted([img for img in os.listdir(self.screenshot_folder) if img.endswith(".jpg")])) + 1
+                i = (
+                    len(
+                        natsorted(
+                            [
+                                img
+                                for img in os.listdir(self.screenshot_folder)
+                                if img.endswith(".jpg")
+                            ]
+                        )
+                    )
+                    + 1
+                )
 
                 while self.__running:
                     screenshot(os.path.join(self.screenshot_folder, f"s{i}.jpg"))
-                    sleep(1/self.fps)
+                    sleep(1 / self.fps)
                     i += 1
 
             else:
-                raise InvalidStartMode("The `self.__start_mode` can only be 'start' or 'resume'.")
+                raise InvalidStartMode(
+                    "The `self.__start_mode` can only be 'start' or 'resume'."
+                )
 
-
-    def start_recording(self, video_name:str="Recording.mp4", fps:int=15) -> None:
+    def start_recording(self, video_name: str = "Recording.mp4", fps: int = 15) -> None:
         """
         Starts screen recording.
 
-        @params 
-        
+        @params
+
         video_name (str) --> The name of the output screen recording.
 
         fps (int) --> The Frames Per Second for the screen recording. Implies how much screenshots will be taken in a second.
         """
-        t = Thread(target=self._start_recording, args=(video_name,fps))
+        t = Thread(target=self._start_recording, args=(video_name, fps))
         t.start()
 
     def stop_recording(self) -> None:
@@ -107,6 +127,8 @@ class ScreenRecorder:
         Stops screen recording.
         """
         if not self.__running:
+            # todo decide the behavior on such cases
+            # whether an error/warning should be raised or ignored
             print("No screen recording session is going on.")
             return None
         self.__running = False
@@ -115,12 +137,13 @@ class ScreenRecorder:
         self._save_video(self.video_name)
         self._clear_data()
 
-
     def pause_recording(self) -> None:
         """
         Pauses screen recording.
         """
         if not self.__running:
+            # todo decide the behavior on such cases
+            # whether an error/warning should be raised or ignored
             print("No screen recording session is going on.")
             return None
 
@@ -131,14 +154,15 @@ class ScreenRecorder:
         Resumes screen recording.
         """
         if self.__running:
+            # todo decide the behavior on such cases
+            # whether an error/warning should be raised or ignored
             print("Screen recording is already running.")
             return None
 
         self.__start_mode = "resume"
         self.start_recording(self.video_name)
 
-
-    def _save_video(self, video_name:str) -> None:
+    def _save_video(self, video_name: str) -> None:
         """
         (Protected) Makes a video out of the screenshots.
 
@@ -147,12 +171,17 @@ class ScreenRecorder:
         video_name (str) --> Name or path to the output video.
         """
         # fetching image info
-        images = natsorted([img for img in os.listdir(self.screenshot_folder) if img.endswith(".jpg")])
+        images = natsorted(
+            [img for img in os.listdir(self.screenshot_folder) if img.endswith(".jpg")]
+        )
+        # print(f"{len(images)=}")
         frame = cv2.imread(os.path.join(self.screenshot_folder, images[0]))
         height, width, _ = frame.shape
 
-        # making a videowriter object 
-        video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (width,height))
+        # making a videowriter object
+        video = cv2.VideoWriter(
+            video_name, cv2.VideoWriter_fourcc(*"mp4v"), self.fps, (width, height)
+        )
 
         # writing all the images to a video
         for image in images:
@@ -172,3 +201,14 @@ class ScreenRecorder:
 
     def __repr__(self) -> str:
         return "pyscreenrec is a small and cross-platform python library that can be used to record screen. \nFor more info, visit https://github.com/shravanasati/pyscreenrec#readme."
+
+
+if __name__ == "__main__":
+    import time
+
+    rec = ScreenRecorder()
+    print("recording started")
+    rec.start_recording(fps=30)
+    time.sleep(10)
+    print("recording ended")
+    rec.stop_recording()
